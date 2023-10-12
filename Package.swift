6e047476b344ae4d49a4617187fb06990352d0cd
SmilesDev <smilesiosteam@gmail.com>
2023-10-12 18:45:23 +0500

    update: youtube player package from branch to version

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/youtube/youtube-ios-player-helper.git", branch: "master"),
+        .package(url:"https://github.com/youtube/youtube-ios-player-helper.git", from: "1.0.4"),
         .package(url: "https://github.com/smilesiosteam/SmilesUtilities.git", branch: "main")
     ],
     targets: [
