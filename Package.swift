0554193c8a287a5fa5191957d1396cc7b381f105
Habib Rehman <haburrehman@eand.com>
2023-10-16 14:07:36 +0500

    -CREATED SHARED_MODELS BRANCH -UPDATED UTILITIES MODULE TO THE SAHRED_MODULES

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url:"https://github.com/youtube/youtube-ios-player-helper.git", from: "1.0.4"),
-        .package(url: "https://github.com/smilesiosteam/SmilesUtilities.git", branch: "main")
+        .package(url: "https://github.com/smilesiosteam/SmilesUtilities.git", branch: "shared_Models")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
